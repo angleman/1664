1fa7968428faf5e5064dca124f6cf83f25248794
angleman <angleman@cormea.com>
2014-03-13 12:10:17 -0600

    expand tests

--- test/basic.js
+++ test/basic.js
@@ -9,4 +9,15 @@ describe('basic', function() {
 		index = require('../index.js')
 		assert(index !== undefined)
     })
+	
+	it('to64 works', function () {
+		base = require('../index.js')
+		assert(base.to64('5d41402abc4b2a76b9719d911017c592') == '1TGK0fk4hfTgamcP4G5xMI')
+	})
+	
+	it('to16 works', function () {
+		base = require('../index.js')
+		assert(base.to16('1TGK0fk4hfTgamcP4G5xMI') == '5d41402abc4b2a76b9719d911017c592')
+	})
 })
+
